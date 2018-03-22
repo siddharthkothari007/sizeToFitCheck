de5bcc3bf3f142a7af5e087acf96f7011a4f5626
Siddharth Kothari <siddharth@agentdesks.com>
2018-03-22 06:53:37 +0530

    Initial Commit

---
+++ sizeToFitCheck/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sizeToFitCheck
+//
+//  Created by Siddharth Kothari on 3/22/18.
+//  Copyright © 2018 KothariSiddharth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
